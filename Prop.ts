7a5cc4f608578a7498afb4971dfc3b5dc8542255
Lolol-Yt <136053748+Lolol-Yt@users.noreply.github.com>
2024-04-20 18:55:57 -0700

    Update Prop.ts

--- Prop.ts
+++ Prop.ts
@@ -1,5 +1,16 @@
-type Sorry<T> = T
-type False = null
-type True = {}
-type Eq<T> = {left: T, right: T}
-type Prop<T> = True | False | Eq<T>
+namespace Prop {
+  export type True = {_propType: "True"}
+  export namespace True {
+    export const intro: () => True = () => {return {_propType: "True"}}
+  }
+  export namespace False {{}}
+  export type And<L,R> = {_propType: "And", left: L, right: R}
+  export namespace And {
+    export function intro<L,R>(left: L, right: R): And<L,R> {return {_propType: "And", left: left, right: right}}
+  }
+  export type Or<L,R> = {_propType: "Or.inl", value: L} | {_propType: "Or.inr", value: R}
+  export namespace Or {
+    export function inl<L,R>(left: L): Or<L,R> {return {_propType: "Or.inl", value: left}}
+    export function inr<L,R>(right: R): Or<L,R> {return {_propType: "Or.inr", value: right}}
+  }
+}
