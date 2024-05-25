d2624e4ba8742bd8db6fea17a23d75d472c0dc86
Lolol-Yt <136053748+Lolol-Yt@users.noreply.github.com>
2024-05-24 21:29:36 -0700

    Update Prop.ts

--- src/Prop.ts
+++ src/Prop.ts
@@ -6,7 +6,9 @@ namespace Prop {
   }
   
   export interface False extends Prop {_propType: "False", proof: never}
-  export namespace False {{/* Todo: make elim*/}}
+  export namespace False {
+    export function elim<C>(f: False) {return f.proof as C}
+  }
   
   export interface And<L extends Prop,R extends Prop> extends Prop {_propType: "And", left: L, right: R}
   export namespace And {
@@ -20,7 +22,7 @@ namespace Prop {
   export namespace Or {
     export function inl<L extends Prop,R extends Prop>(left: L): Or<L,R> {return {_propType: "Or.inl", value: left}}
     export function inr<L extends Prop,R extends Prop>(right: R): Or<L,R> {return {_propType: "Or.inr", value: right}}
-    export function elim<L extends Prop,R extends Prop,C extends Prop>(orProp: Or<L,R>, onLeft: (p: L) => C, onRight: (p: R) => C): C {
+    export function elim<L extends Prop,R extends Prop,C>(orProp: Or<L,R>, onLeft: (p: L) => C, onRight: (p: R) => C): C {
       switch (orProp._propType) {
         case "Or.inl":
           return onLeft(orProp.value)
@@ -35,8 +37,8 @@ namespace Prop {
     export function contradiction<P extends Prop>(p: P, np: Not<P>): False {return np(p)}
   }
   
-  export type Eq<T,A extends T,B extends A> = A extends B ? {_propType: "Eq", left: A, right: B} : never
+  export type Eq<A,B> = A extends B ? (B extends A ? {_propType: "Eq", left: A, right: B} : never) : never
   export namespace Eq {
-    export function rfl<T,A extends T>(a: A): Eq<T,A,A> {return {_propType: "Eq", left: a, right: a} as Eq<T,A,A>}
+    export function rfl<A>(a: A): Eq<A,A> {return {_propType: "Eq", left: a, right: a} as Eq<A,A>}
   }
 }
